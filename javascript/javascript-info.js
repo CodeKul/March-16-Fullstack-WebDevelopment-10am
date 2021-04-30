93a4e18c5021404b5390fff30dbd2fa540712433
sanketcodekul <sanketbhosale.codekul.com>
2021-04-30 11:09:37 +0530

    Javascript Intro

---
+++ javascript/javascript-info.js
@@ -0,0 +1,33 @@
+
+console.log("Welcome to javascript");
+
+
+var number1 = 200.30;
+let name = "Oindrila";
+const PI = 3.14;
+
+// global scope and block scope
+// data types in javascript
+//null, empty and undefined
+
+
+
+
+// PI = 50;
+
+(function showNumber () {
+    var a = 20;
+
+    let name = "Sagar";
+
+    name ="Sanket";
+
+    console.log(number1);
+    console.log(name);
+    console.log(a);
+})();
+
+console.log(a);
+console.log(number1);
+console.log(PI);
+
